0198e270b7af05deadaece7490f9ea423d41147a
Dipika <maharjandipika49@gmail.com>
2026-08-24 15:16:11 +0545

    add VerifyEmail component for email verification process

---
+++ src/vendor/VerifyEmail.jsx
@@ -0,0 +1,33 @@
+import EmailLayout from "../components/EmailLayout";
+import EmailButton from "../components/EmailButton";
+
+function VerifyEmail() {
+  return (
+    <EmailLayout>
+      <article className="verify-email">
+        <p className="verify-eyebrow">VERIFY YOUR EMAIL</p>
+
+        <p className="verify-message">
+          We've sent a verification link to your email address.
+          <br />
+          Please verify your emai to activate your GoDokan account
+          <br />
+          and get started.
+        </p>
+
+        <EmailButton href="https://godokan.com/verify-">
+          CHECK YOUR EMAIL
+        </EmailButton>
+
+        <p className="verify-help">
+          Didn&apos;t receive the email?
+          <br />
+          Check your spam or junk folder, or request a new verification email.
+        </p>
+        <br/>
+      </article>
+    </EmailLayout>
+  );
+}
+
+export default VerifyEmail;
