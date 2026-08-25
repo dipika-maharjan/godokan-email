cde7a030cac89cb561ea998b977b0a7a0b1986a5
iamsabinakhanal <khanalsabina442@gmail.com>
2026-08-25 15:55:13 +0545

    Add email templates for customer interactions and update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import "./App.css";
 import EmailLayout from "./components/EmailLayout";
-import SignupMail from "./Customer/signup-mail";
+import SignUp from "./Customer/signup-mail";
+import VerifyMail from "./Customer/verify-mail";
+import AccountActivated from "./Customer/account-activated";
+import ChangePassword from "./Customer/ChangePassword";
+import CustomerAdded from "./Customer/customer-added";
+import PaymentFailed from "./Customer/paymentFailed";
+import PaymentSuccessful from "./Customer/paymentSuccessful";
+import OrderFailed from "./Customer/OrderFailed";
+import ThanksPlacingOrder from "./Customer/Thanks-placingOrder";
+
+const emailRoutes = {
+  "/customer/signup": SignUp,
+  "/customer/verify-mail": VerifyMail,
+  "/customer/account-activated": AccountActivated,
+  "/customer/change-password": ChangePassword,
+  "/customer/customer-added": CustomerAdded,
+  "/customer/payment-failed": PaymentFailed,
+  "/customer/payment-successful": PaymentSuccessful,
+  "/customer/order-failed": OrderFailed,
+  "/customer/thanks-placing-order": ThanksPlacingOrder,
+};
 
 function App() {
+  const path = window.location.pathname.replace(/\/$/, "") || "/";
+  const Email = emailRoutes[path] || SignUp;
+
   return (
     <EmailLayout>
-      <SignupMail />
+      
     </EmailLayout>
   );
 }
